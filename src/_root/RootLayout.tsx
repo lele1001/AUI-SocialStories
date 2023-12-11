010c97904f8569cb3bf20f2b08499adcdf055af0
Eleonora Cabai <eleonora.cabai@mail.polimi.it>
2023-12-11 07:44:08 +0100

    fixed css

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 
 const RootLayout = () => {
   return (
-    <div className="w-full">
+    <div className="w-full h-fit">
       <TopBar />
       <Outlet />
     </div>
